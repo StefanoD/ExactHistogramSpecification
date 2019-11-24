f957715fcb6b35e2d4540ea9bf35cacd2795555e
Stefano <stefano.d@posteo.de>
2019-11-24 15:25:04 +0100

    Use imageio for reading images instead of scipy.misc

--- example.py
+++ example.py
@@ -6,26 +6,26 @@
 Exact histogram matching
 """
 
-from scipy import misc
+import imageio
 from histogram_matching import ExactHistogramMatcher
 
 
 def histogram_matching_rgb():
-    target_img = misc.imread('images/RGB/Luna_medium.jpg')
-    reference_img = misc.imread('images/RGB/Luna_White_Balance_medium.jpg')
+    target_img = imageio.imread('images/RGB/Luna_medium.jpg')
+    reference_img = imageio.imread('images/RGB/Luna_White_Balance_medium.jpg')
 
     reference_histogram = ExactHistogramMatcher.get_histogram(reference_img)
     new_target_img = ExactHistogramMatcher.match_image_to_histogram(target_img, reference_histogram)
-    misc.imsave('rgb_out.png', new_target_img)
+    imageio.imsave('rgb_out.png', new_target_img)
 
 
 def histogram_matching_grey_values():
-    target_img = misc.imread('images/GreyValue/Luna_medium.jpg')
-    reference_img = misc.imread('images/GreyValue/Luna_White_Balance_medium.jpg')
+    target_img = imageio.imread('images/GreyValue/Luna_medium.jpg')
+    reference_img = imageio.imread('images/GreyValue/Luna_White_Balance_medium.jpg')
 
     reference_histogram = ExactHistogramMatcher.get_histogram(reference_img)
     new_target_img = ExactHistogramMatcher.match_image_to_histogram(target_img, reference_histogram)
-    misc.imsave('grey_out.png', new_target_img)
+    imageio.imsave('grey_out.png', new_target_img)
 
 
 def main():
